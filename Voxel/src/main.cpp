24ce85e0d2bbbd27a1c2a4622be60f0b6e37540d
TomCrypto <thomas.beneteau@yahoo.fr>
2013-11-07 15:45:23 +1300

    Better makefile

--- Voxel/src/main.cpp
+++ Voxel/src/main.cpp
@@ -5,13 +5,17 @@
 
 #include "renderer.hpp"
 
+#ifndef LEAN
 #include "testing/testing.hpp"
+#endif
 
 int main(int argc, char* argv[])
 {
     if ((argc == 2) && (!strcmp(argv[1], "--test")))
     {
+        #ifndef LEAN
         Testing::RunTests();
+        #endif
         return EXIT_SUCCESS;
     }
 
